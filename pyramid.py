a68dc521cbdb4f5767e7ba3738b72b4ea7ba00ca
Shanid <143419556+SH4N1D@users.noreply.github.com>
2024-10-23 14:31:15 +0530

    Add files via upload
    PYTHON PROGRAMS

---
+++ pyramid.py
@@ -0,0 +1,4 @@
+for i in range(0,5):
+    for y in range(0,i):
+        print("*",end=" ")
+    print("")
---
+++ wadiu.py
@@ -0,0 +1,6 @@
+a=int(input("Enter a : "))
+b=int(input("Enter b :"))
+c=a+b
+print(a+b)
+if(c%5==0):
+    print("Divisible by 5")
