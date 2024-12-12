03cae9affdf523f2a5e405d91aeda8b1ba208c6e
Shanid <143419556+SH4N1D@users.noreply.github.com>
2024-12-13 04:58:46 +0530

    Update EX23.PY

--- Python files/RECORD/EX23.PY
+++ Python files/RECORD/EX23.PY
@@ -1,7 +1,13 @@
 import math
 p=[]
+count=0
 for i in range(1000,9999,2):
     a=math.sqrt(i)
     if a%1==0:
-        p.append(i)
-print("The values : ",p)
+        for j in str(i):
+            if int(j)%2==0:
+                count+=1
+        if count==4:
+            p.append(i)
+        count=0
+print("The values :" ,p)
