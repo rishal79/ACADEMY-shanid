a56e913dd7dd6f2fb471082b818c6ca3a5b03bca
Shanid <143419556+SH4N1D@users.noreply.github.com>
2024-12-13 10:09:06 +0530

    Update EX28.py

--- Python files/RECORD/EX28.py
+++ Python files/RECORD/EX28.py
@@ -1,9 +1,11 @@
 a=int(input("Enter the number you want "))
 b=1
 temp=a
-for i in range(1,a+temp):
-    if i<=a:
-        print(i*"*")
-    else:
-        print((temp-b)*"*")
-        b+=1
+for i in range(1):
+    for i in range(1,a+temp):
+        if i<=a:
+            print(i*"*")
+        else:
+            print((temp-b)*"*")
+            b+=1
+    
