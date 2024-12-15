d51a3918da0c2f330691a3ca6a1ca66d2f6babd3
rishal79 <rishalp79@gmail.com>
2024-12-15 14:24:22 +0530

    RECORD PROGRAMS

--- Python files/RECORD/EX32 graphics/main.py
+++ Python files/RECORD/EX32 graphics/main.py
@@ -1,5 +1,5 @@
-from graphics.circle import *
-from graphics.rect import *
+from graphics.circle import cir_area,cir_perimeter
+from graphics.rect import recta_area as r_area,recta_perimeter as r_perimeter
 from graphics.dgraphics.sphere import *
 from graphics.dgraphics.cuboid import *
 
@@ -18,3 +18,19 @@ print(f"Sphere Perimeter: {sphere_perimeter(3)}")
 # Cuboid
 print(f"Cuboid Area: {cub_area(2, 3, 4)}")
 print(f"Cuboid Perimeter: {cub_perimeter(2, 3, 4)}")
+
+
+
+# Step 1: Start
+# Step 2: Create a package graphics with modules:
+        # - rectangle: Methods for area and perimeter of a rectangle.
+        # - circle: Methods for area and perimeter of a circle.
+# Step 3: Create a sub-package 3D-graphics with modules:
+        # - cuboid: Methods for surface area and perimeter (edge length sum) of a cuboid.
+        # - sphere: Methods for surface area and circumference of a sphere.
+# Step 4: Write a program to import modules using these methods:
+        # Specific Method Import: Use from <module> import <method> to import specific methods.
+        # Alias Import: Use from <module> import <method> as <alias> 
+        # Import All Methods: Use from <module> import * to access all methods directly.
+# Step 5: Call methods to calculate and print area and perimeter for each figure.
+# Step 6: Stop
