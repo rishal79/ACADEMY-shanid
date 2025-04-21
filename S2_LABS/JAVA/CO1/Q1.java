57812ac086e78f7db3c5a0d323a7d3c8ffc70391
Shanid <143419556+SH4N1D@users.noreply.github.com>
2025-04-21 22:54:30 +0530

    Update Q1.java

--- S2_LABS/JAVA/CO1/Q1.java
+++ S2_LABS/JAVA/CO1/Q1.java
@@ -1,6 +1,7 @@
 Q)Define a class ‘product’ with data members pcode, pname and price. Create 3 objects of 
 the class and find the product having the lowest price
 
+code:
 
 import java.util.Scanner;
 class Product{
