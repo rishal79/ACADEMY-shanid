7d966fe1f46bd89a8885ac1a7437fd2f65cde79a
RISHAL P <rishalp79@gmail.com>
2025-05-22 14:11:18 +0530

    Update Q19.sh

--- S2_LABS/SHELL/RECORD/Q19.sh
+++ S2_LABS/SHELL/RECORD/Q19.sh
@@ -2,19 +2,20 @@ Q)Shell script to display the content of a directory file in windows style
 
 
 Code:
-echo "\n\n Directory of `pwd` \n"
+echo -e "\n\n Directory of `pwd` \n"
 for f in *
 do
 #dpart=`ls -l $f | cut -d ' ' -f6,7`
 fdatetime=`date -r $f "+%d-%m-%y %H:%M:%S"`
 if [ -d $f ]
 then
-echo "$f \t\t<DIR>\t\t $fdatetime"
+echo -e "$f \t\t<DIR>\t\t $fdatetime"
 else
 f_size=`cat $f | wc -c`
-echo "$f \t\t $f_size \t $fdatetime"
+echo -e "$f \t\t $f_size \t $fdatetime"
 fi
 done
+
 
 Algorithm:
 Step 1: Show current directory path.
@@ -30,12 +31,8 @@ Input:
 sh window.sh file1.txt
 
 Output:
-\n\n Directory of /c/Users/PROGRAMMING LAB 2/desktop/anjala \n
-1.sh \t\t 27 \t 16-05-25 12:03:00
-a.sh \t\t 24 \t 16-05-25 12:02:49
-a.sh.rev \t\t 24 \t 16-05-25 14:20:10
-b.sh \t\t 14 \t 16-05-25 12:12:14
-c.sh \t\t 23 \t 16-05-25 15:16:58
-c.sh.rev \t\t 23 \t 16-05-25 15:15:56
-compare.sh \t\t 291 \t 16-05-25 14:53:51
-content.sh \t\t 268 \t 16-05-25 15:36:13
+ Directory of /c/Users/MCA19/Desktop/New folder
+
+A.SH.1610                225     22-05-25 13:35:05
+b.sh                     265     22-05-25 14:09:00
+
