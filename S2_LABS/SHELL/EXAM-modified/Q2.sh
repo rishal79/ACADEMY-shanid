3181ef2319df56af52297e4efd016b89e3dc51c5
Shanid <143419556+SH4N1D@users.noreply.github.com>
2025-06-09 23:24:48 +0530

    Delete S2_LABS/SHELL/EXAM-modified/Q2.sh

--- S2_LABS/SHELL/EXAM-modified/Q2.sh
+++
@@ -1,7 +0,0 @@
-read -p "Enter the 3 sides of the Triangle : " a b c
-if (( $a + $b <= $c || $b + $c<=$a || $c + $a <=$b ))
-then 
-	echo "Triangle cannot be formed"
-else
-	echo "Triangle can be formed"
-fi
