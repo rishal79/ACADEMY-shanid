0d9b5daab87cdfc678b0ea6e7e631519214af297
Shanid <143419556+SH4N1D@users.noreply.github.com>
2025-06-09 22:46:09 +0530

    Create Q4.sh

---
+++ S2_LABS/SHELL/EXAM/Q4.sh
@@ -0,0 +1,18 @@
+Q) Write a shell script using command line to test whether the given number is prime or not.
+
+
+Code:
+if [ $# -ne 1 ]
+then
+        echo "Syntax is $0 <value>"
+fi
+n=( $1 /2 )
+for (( i=2;i<n;i++))
+do
+        if (( $1 % i == 0 ))
+        then
+                echo "$1 is not a  prime number "
+                exit 1
+        fi
+done
+echo "$1 is prime number"
