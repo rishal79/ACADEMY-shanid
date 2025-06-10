da7d874cbba112ea9ed35e87c73630fc0fc2de8a
Shanid <143419556+SH4N1D@users.noreply.github.com>
2025-06-10 06:25:46 +0530

    Update Q1.sh

--- S2_LABS/SHELL/EXAM-modified/Q1.sh
+++ S2_LABS/SHELL/EXAM-modified/Q1.sh
@@ -2,7 +2,7 @@ read -p "Enter the year : " year
 if (( $year %400 == 0 ))
 then
 	echo "$year is a leap year"
-elif (( $year % 4 == 0  &&  $year %100 == 0 ))
+elif (( $year % 4 == 0  &&  $year %100 != 0 ))
 then
 	echo "$year is a leap year"
 else
